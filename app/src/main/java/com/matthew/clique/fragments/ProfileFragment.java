b7a46fa4dea2efe3b302cc1b19254560dd2ab4b7
Matthew Ouyang <mouyang2001@gmail.com>
2020-01-03 20:49:53 +1300

    image upload complete

--- app/src/main/java/com/matthew/clique/fragments/ProfileFragment.java
+++ app/src/main/java/com/matthew/clique/fragments/ProfileFragment.java
@@ -8,9 +8,11 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,15 +24,26 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.matthew.clique.MainActivity;
 import com.matthew.clique.R;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -44,11 +57,14 @@ public class ProfileFragment extends Fragment {
 
     private CircleImageView profileImage;
     private TextView profileNameField;
+    private ProgressBar progressBar;
 
     private FirebaseFirestore firebaseFirestore;
     private FirebaseAuth firebaseAuth;
+    private StorageReference storage;
 
     private String userName;
+    private String userId;
 
     private Uri profileImageUri = null;
 
@@ -59,9 +75,13 @@ public class ProfileFragment extends Fragment {
 
         profileImage = view.findViewById(R.id.circleImageViewProfileImage);
         profileNameField = view.findViewById(R.id.textViewProfileName);
+        progressBar = view.findViewById(R.id.progressBarProfile);
 
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseFirestore = FirebaseFirestore.getInstance();
+        storage = FirebaseStorage.getInstance().getReference();
+
+        userId = firebaseAuth.getUid();
 
         final MainActivity mainActivity = (MainActivity)getActivity();
         firebaseFirestore
@@ -86,16 +106,12 @@ public class ProfileFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    if (ContextCompat.checkSelfPermission(getActivity()
-                            , Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                        ActivityCompat.requestPermissions(getActivity(),
-                                new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
-                                1);
+                    if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
                     } else {
                         CropImage.activity()
-                                .setGuidelines(CropImageView.Guidelines.ON)
-                                .setAspectRatio(1, 1)
-                                .start(getActivity());
+                                .setAspectRatio(1,1)
+                                .start(getContext(), ProfileFragment.this);
 
                     }
                 }
@@ -110,11 +126,47 @@ public class ProfileFragment extends Fragment {
         if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
             if (resultCode == RESULT_OK) {
-                profileImageUri = result.getUri();
-                profileImage.setImageURI(profileImageUri);
+                Uri resultUri = result.getUri();
+                profileImage.setImageURI(resultUri);
+
+                StorageReference imagePath = storage.child("profile_images").child(userId+".jpg");
+                uploadImage(imagePath, resultUri);
+
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
                 Exception error = result.getError();
             }
         }
+    }
+
+    private void uploadImage(StorageReference path, Uri uri) {
+        final StorageReference imagePath = path;
+        imagePath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                imagePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Map<String, Object> imageMap = new HashMap<>();
+                        imageMap.put("image_url", uri.toString());
+                        imageMap.put("uploaded", FieldValue.serverTimestamp());
+                        firebaseFirestore
+                                .collection("Users/" + userId + "/Pictures")
+                                .document("profile_picture")
+                                .set(imageMap)
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            Toast.makeText(getActivity(), "Profile image changed", Toast.LENGTH_SHORT).show();
+                                        } else {
+                                            String e = task.getException().getMessage();
+                                            Toast.makeText(getActivity(), e, Toast.LENGTH_LONG).show();
+                                        }
+                                    }
+                                });
+                    }
+                });
+            }
+        });
     }
 }
